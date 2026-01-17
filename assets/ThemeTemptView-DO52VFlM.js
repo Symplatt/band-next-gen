2e762cd395b24f90d5ccf86f4975eb1032f9515e
chenjz31026 <c1624931494@gmail.com>
2026-01-17 22:20:10 +0800

    Updates

---
+++ assets/ThemeTemptView-DO52VFlM.js
@@ -0,0 +1,138 @@
+import{M as n}from"./MarkDownViewer-BENXnwiB.js";import{d as r,c as i,f as s,h as e,s as t,e as a}from"./index-VjlD91PC.js";const o=`# 一级标题\r
+\r
+## 二级标题\r
+\r
+### 三级标题\r
+\r
+#### 四级标题\r
+\r
+##### 五级标题\r
+\r
+###### 六级标题\r
+\r
+这是正文字体是思源黑体这是正文字体是思源黑体这是正文字体是思源黑体这是正文字体是思源黑体这是正文字体是思源黑体这是正文字体是思源黑体这是正文字体是思源黑体这是正文字体是思源黑体这是正文字体是思源黑体这是正文字这是正文字体是思源黑体这是正文字体是思源黑体这是正文字体是思源黑体这是正文字体是思源黑体体是这是正文字体是思源黑体这是正文字体是思源黑体思源黑体这是正文字体是思源黑体这是正文字体是思源黑体这是正文字体是思源黑体这是正文字\r
+\r
+思源黑体有个好处，就是"英文引号"和“中文引号”区别很明显。\r
+\r
+\`\`\`c\r
+// 这是代码块\r
+int main() {\r
+	printf("hello world\\n");\r
+	return 0;\r
+}\r
+// 如果是consolas字体，就能清晰分辨：数字0 字母O 大写L 小写l 大写I 小写1 阿拉伯数字1\r
+\`\`\`\r
+\r
+- [ ] 这是一段行内代码：\`字体和代码快一致，数字0 字母O 大写L 小写l 大写I 小写1 阿拉伯数字1\` \r
+- [ ] <u>这是一段有下划线的文字</u>\r
+\r
+1. 这是**加粗**\r
+2. 这是*倾斜*\r
+3. 这是==标黄==\r
+\r
+- 这是~~删除线~~\r
+\r
+- 这是上标^这是上标^ \r
+- 这是下标~这是下标~ \r
+\r
+| 这是               | 表格                           | 的表头   |\r
+| ------------------ | ------------------------------ | -------- |\r
+| 阿迪发萨斯         | 暗室逢灯                       |          |\r
+| 阿发啊啊啊啊啊啊啊 | 阿凡达啊啊啊啊啊啊啊啊啊啊啊啊 | 阿斯顿发 |\r
+| 阿发               | aadsfasfdsdadfas1100000000     |          |\r
+\r
+> 这是一段引用这是一段引用这是一段引用这是一段引用这是一段引用这是一段引用这是一段引用这是一段引用这是一段引用这是一段引用这是一段引用。\r
+>\r
+> \`\`\`c\r
+> int main() {\r
+> 	printf("hello world\\n");\r
+> 	return 0;\r
+> }\r
+> \`\`\`\r
+>\r
+> ------\r
+>\r
+> 1. 这是**加粗**\r
+> 2. 这是*倾斜*\r
+> 3. 这是==标黄==\r
+> 4. 这是~~删除线~~\r
+>\r
+> - [ ] 这是一段行内代码：\`字体和代码快一致，数字0 字母O 大写L 小写l 大写I 小写1 阿拉伯数字1\` \r
+> - [ ] <u>这是一段有下划线的文字</u>\r
+> - 这是上标^这是上标^ \r
+> - 这是下标~这是下标~ \r
+\r
+\r
+\r
+------\r
+\r
+\r
+\r
+# Level 1 Heading\r
+\r
+## Level 2 Heading\r
+\r
+### Level 3 Heading\r
+\r
+#### Level 4 Heading\r
+\r
+##### Level 5 Heading\r
+\r
+###### Level 6 Heading\r
+\r
+This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text in Source Han Sans. This is body text.\r
+\r
+One advantage of **Source Han Sans** is that the distinction between "English quotation marks" and “Chinese quotation marks” is very clear.\r
+\r
+\`\`\`c\r
+// This is a code block\r
+int main() {\r
+    printf("hello world\\n");\r
+    return 0;\r
+}\r
+// If the font is Consolas, you can clearly distinguish:\r
+// digit 0, letter O, uppercase L, lowercase l, uppercase I, lowercase l, digit 1\r
+\`\`\`\r
+\r
+-  This is an inline code segment: \`The font matches the code block, digit 0, letter O, uppercase L, lowercase l, uppercase I, lowercase l, digit 1\`\r
+-  This is underlined text\r
+\r
+1. This is **bold**\r
+2. This is *italic*\r
+3. This is ==highlighted==\r
+\r
+- This is ~~strikethrough~~\r
+- This is superscript^this is superscript^\r
+- This is subscript~this is subscript~\r
+\r
+| This is          | a table                         | header |\r
+| ---------------- | ------------------------------- | ------ |\r
+| Random text      | A candle in a dark room         |        |\r
+| Long placeholder | Avatar with very very long text | Sample |\r
+| Short            | aadsfasfdsdadfas1100000000      |        |\r
+\r
+> This is a blockquote. This is a blockquote. This is a blockquote. This is a blockquote. This is a blockquote. This is a blockquote. This is a blockquote. This is a blockquote. This is a blockquote. This is a blockquote. This is a blockquote.\r
+>\r
+> \`\`\`c\r
+> int main() {\r
+>     printf("hello world\\n");\r
+>     return 0;\r
+> }\r
+> \`\`\`\r
+>\r
+> ------\r
+>\r
+> 1. This is **bold**\r
+> 2. This is *italic*\r
+> 3. This is ==highlighted==\r
+> 4. This is ~~strikethrough~~\r
+>\r
+> - [ ]  This is inline code: \`The font matches the code block, digit 0, letter O, uppercase L, lowercase l, uppercase I, lowercase l, digit 1\`\r
+> - [ ]  This is underlined text\r
+>\r
+> - This is superscript^this is superscript^\r
+> - This is subscript~this is subscript~\r
+\r
+\r
+\r
+`,c={class:"about-view"},h={class:"page-container"},p=r({__name:"ThemeTemptView",setup(l){return(d,u)=>(a(),i("div",c,[s("div",h,[e(n,{content:t(o)},null,8,["content"])])]))}});export{p as default};
