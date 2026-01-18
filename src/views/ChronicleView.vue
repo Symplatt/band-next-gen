cdb6e375d3185cfffd6a0b30f176172d6a603433
chenjz31026 <c1624931494@gmail.com>
2026-01-18 11:20:09 +0800

    update

--- src/views/ChronicleView.vue
+++ src/views/ChronicleView.vue
@@ -1,18 +1,21 @@
 // views/CharactersView.vue
 
 <template>
-  <span style="padding-left: 200px; color: aliceblue">页面开发中……</span>
+  <div class="chronicle-view">
+    <p>该模块用于呈现子世代完整时间轴，</p>
+    <p>具体内容暂且搁置，有待后续开发。</p>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
 
 <style lang="css" scoped>
   .chronicle-view {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-    width: 100%;
-    height: 100%;
+    height: 90vh;
+    margin-top: 100px;
+    font-family: 'Noto Serif SC', serif;
+    font-size: 2rem;
+    color: #fff;
+    text-align: center;
   }
 </style>
