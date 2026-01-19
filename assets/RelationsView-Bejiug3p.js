5e1899169fdccc359e0d762165c227d5296de101
chenjz31026 <c1624931494@gmail.com>
2026-01-19 18:38:20 +0800

    Updates

---
+++ assets/RelationsView-Bejiug3p.js
@@ -0,0 +1,75 @@
+import{d as R,k as z,r as _,o as A,b as H,c as o,f as e,F as u,i as d,n as m,t as h,m as V,q,e as i,_ as D}from"./index-QBMDNzdV.js";const F=[{groupName:"Ex-Lilith",members:[{id:"hazuki",name:"丰川羽月",avatar:"images/avatar/hazuki.png"},{id:"riko",name:"八幡莉子",avatar:"images/avatar/riko.png"},{id:"ameha",name:"若叶天叶",avatar:"images/avatar/ameha.png"},{id:"wakaho",name:"若叶若穗",avatar:"images/avatar/wakaho.png"}]},{groupName:"Go Raiiii",members:[{id:"mirei",name:"椎名美铃",avatar:"images/avatar/mirei.png"},{id:"shizuku",name:"高松雫",avatar:"images/avatar/shizuku.png"},{id:"anna",name:"长崎杏奈",avatar:"images/avatar/anna.png"},{id:"muna",name:"若叶梦那",avatar:"images/avatar/muna.png"}]},{groupName:"Other",members:[{id:"sora",name:"千早空",avatar:"images/avatar/sora.png"}]}],Y={hazuki:{riko:`莉子同学(riko san)
+莉子(riko)`,ameha:`天叶
+(ameha)`,wakaho:`若穗
+(wakaho)`,mirei:`小美铃
+(mirei chan)`,shizuku:`小雫
+(shizuku chan)`,anna:`小杏奈
+(anna chan)`,muna:`小梦那
+(muna chan)`,sora:`小空
+(sora chan)`},riko:{hazuki:`丰川同学/羽月同学
+(togawa san/hazuki san)`,ameha:`天叶
+(ameha)`,wakaho:`若穗
+(wakaho)`,mirei:`喂/美铃
+(mirei)`,shizuku:`雫雫
+(ルル ruru)`,anna:`小安
+(ann chan)`,muna:`若叶同学
+(wakaba san)`,sora:`小空
+(sora chan)`},ameha:{hazuki:`小羽月
+(hazuki chan)`,riko:`莉子/小栗子
+(riko/koriko)`,wakaho:`若
+(waka)`,mirei:`小美铃
+(mirei chan)`,shizuku:`雫雫
+(ルル ruru chan)`,anna:`小杏奈
+(anna chan)`,muna:`小梦那
+(muna chan)`,sora:`小空
+(sora chan)`},wakaho:{hazuki:`月
+(zuki)`,riko:`莉子
+(riko)`,ameha:`天
+(ame)`,mirei:`美铃铃
+(mirin)`,shizuku:`雫雫
+(ルル ruru)`,anna:`杏奈
+(anna)`,muna:`梦那
+(muna)`,sora:`小空
+(sora chan)`},shizuku:{hazuki:`小月
+(zuki chan)`,riko:`姐姐/莉子
+(neesan/riko)`,ameha:`小天
+(ame chan)`,wakaho:`小若
+(waka chan)`,mirei:`美铃铃
+(mirin chan)`,anna:`小安
+(ann chan)`,muna:`小猫
+(nyanko chan)`,sora:`小空
+(sora chan)`},mirei:{hazuki:`羽月姐姐
+(hazuki san)`,riko:`栗子/姐姐
+(koriko/nee)`,ameha:`天叶
+(ameha chan)`,wakaho:`小若穗
+(wakaho chan)`,shizuku:`雫雫
+(ルル ruru)`,anna:`小安
+(ann)`,muna:`梦那
+(muna)`,sora:`小空
+(sora chan)`},anna:{hazuki:`羽月姐姐
+(hazuki san)`,riko:`姐姐/莉子姐姐
+(oneesan/riko san)`,ameha:`天叶
+(ameha chan)`,wakaho:`小若穗
+(wakaho chan)`,shizuku:`雫雫
+(ルル ruru chan)`,mirei:`美铃
+(mirin)`,muna:`小梦那
+(muna chan)`,sora:`小空
+(sora chan)`},muna:{hazuki:`羽月姐姐
+(hazuki san)`,riko:`姐姐/莉子姐姐
+(oneesan/riko san)`,ameha:`天姐
+(ame nee)`,wakaho:`若姐
+(waka nee)`,shizuku:`小雫
+(shizuku chan)`,mirei:`美子
+(mi ko)`,anna:`杏子
+(ann ko)`,sora:`小空
+(sora chan)`},sora:{hazuki:`姐姐/羽月姐姐
+(oneesan/hazuki san)`,riko:`姐姐/莉子姐姐
+(oneesan/riko san)`,ameha:`姐姐/天叶姐姐
+(oneesan/ameha san)`,wakaho:`姐姐/若穗姐姐
+(oneesan/wakaho san)`,shizuku:`雫雫
+(ルル ruru chan)`,mirei:`姐姐/美铃姐姐
+(oneesan/mirei san)`,anna:`姐姐
+(neesan)`,muna:`姐姐/梦那姐姐
+(oneesan/muna san)`}},G=[{child:[{id:"hazuki",name:"丰川羽月",avatar:"images/avatar/hazuki.png"}],mother:[{name:"丰川祥子",avatar:"images/avatar/sakiko.png"},{name:"三角初华",avatar:"images/avatar/uika.png"}],type:"birth"},{child:[{id:"riko",name:"八幡莉子",avatar:"images/avatar/riko.png"},{id:"mirei",name:"椎名美铃",avatar:"images/avatar/mirei.png"}],mother:[{name:"八幡海铃",avatar:"images/avatar/umiri.png"},{name:"椎名立希",avatar:"images/avatar/taki.png"}],type:"birth"},{child:[{id:"ameha",name:"若叶天叶",avatar:"images/avatar/ameha.png"},{id:"wakaho",name:"若叶若穗",avatar:"images/avatar/wakaho.png"},{id:"muna",name:"若叶梦那",avatar:"images/avatar/muna.png"}],mother:[{name:"祐天寺若麦",avatar:"images/avatar/nyamu.png"},{name:"若叶睦",avatar:"images/avatar/mutsumi.png"}],type:"birth"},{child:[{id:"anna",name:"长崎杏奈",avatar:"images/avatar/anna.png"},{id:"sora",name:"千早空",avatar:"images/avatar/sora.png"}],mother:[{name:"长崎素世",avatar:"images/avatar/soyo.png"},{name:"千早爱音",avatar:"images/avatar/anon.png"}],type:"birth"},{child:{id:"shizuku",name:"高松雫",avatar:"images/avatar/shizuku.png"},mother:[{name:"高松灯",avatar:"images/avatar/tomori.png"},{name:"要乐奈",avatar:"images/avatar/rana.png"}],type:"adopt"}],b={characters:F,callTable:Y,familyTree:G},U={class:"relations-view"},W={class:"sidebar"},j={class:"nav-list"},J=["onClick"],K={class:"label"},Q={class:"page-container"},X={id:"section-table",class:"section-block"},Z={class:"table-scroll-wrapper"},aa={class:"call-table"},na={class:"header-avator"},ea=["src"],sa={class:"header-char"},ta={class:"th-content"},oa={class:"td-content"},ia=["onMouseenter"],ra={key:0,class:"call-content"},la={class:"romaji"},ca={key:1,class:"dash"},ua={id:"section-network",class:"section-block"},da={class:"network-container desktop-only"},ha={class:"circle-layout"},ma={class:"lines-layer",viewBox:"0 0 500 500"},va=["x1","y1","x2","y2"],ka=["onMouseenter"],ga={class:"node-circle"},pa={class:"node-text"},_a={id:"section-family",class:"section-block"},wa={class:"genealogy-grid"},fa={class:"parents-row"},ya=["src"],za={class:"name"},ba={class:"child-row"},xa=["src"],Ea={class:"name"},Ma=R({__name:"RelationsView",setup(Ca){const L="/band-next-gen/",w=s=>{if(!s)return"";const n=s.startsWith("/")?s.slice(1):s;return`${L}${n}`},c=z(()=>b.characters.flatMap(s=>s.members)),S=b.callTable,v=_(null),k=_(null),T=(s,n)=>{v.value=s,k.value=n},N=()=>{v.value=null,k.value=null},x=(s,n)=>S[s]?.[n]||"?",f=_("section-table"),E=[{id:"section-table",label:"称呼表"},{id:"section-network",label:"关系网"},{id:"section-family",label:"亲缘谱"}],P=s=>{f.value=s;const n=document.getElementById(s);if(n){const a=getComputedStyle(document.documentElement).getPropertyValue("--header-height").trim(),r=parseInt(a||"0")+0,y=n.getBoundingClientRect().top+window.pageYOffset-r-20;window.scrollTo({top:y,behavior:"smooth"})}},M=()=>{const n=window.scrollY;for(const a of E){const t=document.getElementById(a.id);if(t){if(window.innerWidth<=768&&a.id==="section-network")continue;const{offsetTop:r,offsetHeight:p}=t;n>=r-300&&n<r+p-300&&(f.value=a.id)}}};A(()=>{window.addEventListener("scroll",M);const s=new IntersectionObserver(n=>{n.forEach(a=>{a.isIntersecting&&(a.target.classList.add("visible"),s.unobserve(a.target))})},{threshold:.1});document.querySelectorAll(".section-title").forEach(n=>s.observe(n))}),H(()=>{window.removeEventListener("scroll",M)});const l=_(null),C=z(()=>{const s=[],n=c.value.map(a=>a.id);return n.forEach((a,t)=>{t<n.length-1&&s.push({source:a,target:n[t+1]}),t<n.length-2&&s.push({source:a,target:n[t+2]}),t===n.length-1&&s.push({source:a,target:n[0]})}),s}),g=s=>{const n=c.value.findIndex(y=>y.id===s);if(n===-1)return{x:250,y:250};const a=c.value.length,t=180,p=(360/a*n-90)*Math.PI/180;return{x:250+Math.cos(p)*t,y:250+Math.sin(p)*t}},$=(s,n)=>{const a=c.value[s];if(!a)return{};const t=g(a.id);return{left:`${t.x}px`,top:`${t.y}px`}},B=s=>{if(!l.value)return!0;const n=s===l.value,a=C.value.some(t=>t.source===l.value&&t.target===s||t.target===l.value&&t.source===s);return n||a},I=s=>l.value?s.source===l.value||s.target===l.value:!0,O=z(()=>b.familyTree.map(s=>({...s,child:Array.isArray(s.child)?s.child:[s.child]})));return(s,n)=>(i(),o("div",U,[e("nav",W,[n[2]||(n[2]=e("div",{class:"nav-line"},null,-1)),e("ul",j,[(i(),o(u,null,d(E,(a,t)=>e("li",{key:t,class:m(["nav-item",{active:f.value===a.id}]),onClick:r=>P(a.id)},[n[1]||(n[1]=e("span",{class:"dot"},null,-1)),e("span",K,h(a.label),1)],10,J)),64))])]),e("div",Q,[e("section",X,[n[6]||(n[6]=e("h2",{class:"section-title"},[e("span",null,"称呼表")],-1)),n[7]||(n[7]=e("div",{class:"table-description"},"表格可横向滚动，但更适合电脑浏览器查看",-1)),e("div",Z,[e("table",aa,[e("thead",null,[e("tr",na,[n[3]||(n[3]=e("th",{class:"fixed-col"},null,-1)),(i(!0),o(u,null,d(c.value,a=>(i(),o("th",{key:a.id},[e("img",{src:w(a.avatar),class:"avatar-square",loading:"lazy"},null,8,ea)]))),128))]),n[5]||(n[5]=e("tr",{class:"header-group"},[e("th",{class:"fixed-col"},[e("span",{class:"group-label"},"所属乐队")]),e("th",{colspan:"4",class:"group-name lilith"},"Ex-Lilith"),e("th",{colspan:"4",class:"group-name gorai"},"Go Raiiii"),e("th",{colspan:"1",class:"group-name other"},"Other")],-1)),e("tr",sa,[n[4]||(n[4]=e("th",{class:"corner-header fixed-col"},[e("span",{class:"corner-comment"},"称呼→被称呼")],-1)),(i(!0),o(u,null,d(c.value,a=>(i(),o("th",{key:a.id,class:m({"highlight-col":k.value===a.id})},[e("div",ta,h(a.name),1)],2))),128))])]),e("tbody",null,[(i(!0),o(u,null,d(c.value,a=>(i(),o("tr",{key:a.id},[e("td",{class:m(["row-header fixed-col",{"highlight-row":v.value===a.id}])},[e("div",oa,h(a.name),1)],2),(i(!0),o(u,null,d(c.value,t=>(i(),o("td",{key:t.id,class:m(["cell",{active:v.value===a.id&&k.value===t.id,"cross-highlight":v.value===a.id||k.value===t.id,self:a.id===t.id}]),onMouseenter:r=>T(a.id,t.id),onMouseleave:N},[a.id!==t.id?(i(),o("div",ra,[V(h(x(a.id,t.id).split(`
+`)[0])+" ",1),e("span",la,h(x(a.id,t.id).split(`
+`)[1]||""),1)])):(i(),o("span",ca,"—"))],42,ia))),128))]))),128))])])])]),e("section",ua,[n[8]||(n[8]=e("h2",{class:"section-title"},[e("span",null,"关系网")],-1)),e("div",da,[e("div",ha,[(i(),o("svg",ma,[(i(!0),o(u,null,d(C.value,(a,t)=>(i(),o("line",{key:t,x1:g(a.source).x,y1:g(a.source).y,x2:g(a.target).x,y2:g(a.target).y,class:m(["network-line",{dimmed:l.value&&!I(a),active:I(a)}])},null,10,va))),128))])),(i(!0),o(u,null,d(c.value,(a,t)=>(i(),o("div",{key:a.id,class:m(["node-wrapper",{dimmed:l.value&&!B(a.id),active:l.value===a.id}]),style:q($(t,c.value.length)),onMouseenter:r=>l.value=a.id,onMouseleave:n[0]||(n[0]=r=>l.value=null)},[e("div",ga,[e("span",pa,h(a.name.slice(-2)),1)])],46,ka))),128))])]),n[9]||(n[9]=e("div",{class:"mobile-network-msg"},[e("p",null,"正在进行手机端适配，暂不显示，请使用电脑端浏览器查看。")],-1))]),e("section",_a,[n[11]||(n[11]=e("h2",{class:"section-title"},[e("span",null,"亲缘谱")],-1)),e("div",wa,[(i(!0),o(u,null,d(O.value,(a,t)=>(i(),o("div",{key:t,class:"family-unit"},[e("div",fa,[(i(!0),o(u,null,d(a.mother,r=>(i(),o("div",{key:r.name,class:"parent-node"},[e("img",{src:w(r.avatar),class:"avatar-round",loading:"lazy"},null,8,ya),e("span",za,h(r.name),1)]))),128))]),e("div",{class:m(["connector",{"dashed-line":a.type==="adopt"}])},[...n[10]||(n[10]=[e("div",{class:"connector-top"},null,-1),e("div",{class:"connector-bottom"},null,-1)])],2),e("div",ba,[(i(!0),o(u,null,d(a.child,r=>(i(),o("div",{key:r.id,class:"child-node"},[e("img",{src:w(r.avatar),class:"avatar-round",loading:"lazy"},null,8,xa),e("span",Ea,h(r.name),1)]))),128))])]))),128))])])])]))}}),La=D(Ma,[["__scopeId","data-v-2ff88e2a"]]);export{La as default};
