bdb0f58fc6d4dfa3b7fa9945884e4fbf05487112
chenjz31026 <c1624931494@gmail.com>
2026-01-21 19:24:21 +0800

    Updates

--- assets/AboutView-CnpQcPR-.js
+++ assets/AboutView-CnpQcPR-.js
@@ -1,4 +1,4 @@
-import{M as r}from"./MarkDownViewer-DarsbR1z.js";import{d as a,c as o,f as n,h as t,q as e,e as s,_ as c}from"./index-TJ4MmN-U.js";const i=`## 项目介绍\r
+import{M as r}from"./MarkDownViewer-Drhm8g0Z.js";import{d as a,c as o,f as n,h as t,q as e,e as s,_ as c}from"./index-D4Y6j1X4.js";const i=`## 项目介绍\r
 \r
 「乐队子世代：那美好的未来」是由画师**鹤星**基于《BanG Dream! It's MyGO!!!!!》（迷途之子）与《BanG Dream! Ave Mujica》（颂乐人偶）创作的长篇、多形式系列二次创作。\r
 \r
